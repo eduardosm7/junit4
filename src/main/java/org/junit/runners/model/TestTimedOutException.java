349efa5f7489c7f14d63edf248561aea3efee7c7
eduardosm7 <esm7@cin.ufpe.br>
2018-11-11 13:29:46 -0300

    Add JML to src/main/java/org/junit/runners/model/TestTimedOutException.java

--- src/main/java/org/junit/runners/model/TestTimedOutException.java
+++ src/main/java/org/junit/runners/model/TestTimedOutException.java
@@ -10,10 +10,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestTimedOutException extends Exception {
 
-    private static final long serialVersionUID = 31935685163547539L;
+    private /*@ spec_public @*/ static final long serialVersionUID = 31935685163547539L;
 
-    private final TimeUnit timeUnit;
-    private final long timeout;
+    private /*@ spec_public @*/ final TimeUnit timeUnit;
+    private /*@ spec_public @*/ final long timeout;
+
+    //@ invariant this.serialVersionUID == 31935685163547539L;
 
     /**
      * Creates exception with a standard message "test timed out after [timeout] [timeUnit]"
@@ -21,6 +23,8 @@ public class TestTimedOutException extends Exception {
      * @param timeout the amount of time passed before the test was interrupted
      * @param timeUnit the time unit for the timeout value
      */
+    //@ ensures this.timeout == timeout;
+    //@ ensures this.timeUnit == timeUnit;
     public TestTimedOutException(long timeout, TimeUnit timeUnit) {
         super(String.format("test timed out after %d %s", 
                 timeout, timeUnit.name().toLowerCase()));
@@ -31,6 +35,7 @@ public class TestTimedOutException extends Exception {
     /**
      * Gets the time passed before the test was interrupted
      */
+    //@ ensures \result == this.timeout;
     public long getTimeout() {
         return timeout;
     }
@@ -38,6 +43,7 @@ public class TestTimedOutException extends Exception {
     /**
      * Gets the time unit for the timeout value
      */
+    //@ ensures \result == this.timeUnit;
     public TimeUnit getTimeUnit() {
         return timeUnit;
     }
